672f5ec592ef6b8fa589e44901b7bc878185d8a9
Ashwani Yadav <ashyadavash@gmail.com>
2020-03-11 16:31:00 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+Steps to use:-
+
+(1) npm install
+(2) npm start
+(3) Open 1st page using localhost:${port}/#init
+(4) Open 2nd pages using localhost:${port}/
+(5) Enter 'yourId' in others 'otherId' and cick connect
+(6) Enter others 'yourId' in your 'otherId' and click connect
+(&) You're done
---
+++ index.js
@@ -0,0 +1,39 @@
+var getUserMedia = require('getusermedia')
+
+getUserMedia({ video: true, audio: false }, function (err, stream) {
+  if (err) return console.error(err)
+
+  var Peer = require('simple-peer')
+  var peer = new Peer({
+    initiator: location.hash === '#init',
+    trickle: false,
+    stream: stream
+  })
+
+  peer.on('signal', function (data) {
+    document.getElementById('yourId').value = JSON.stringify(data)
+  })
+
+  document.getElementById('connect').addEventListener('click', function () {
+    var otherId = JSON.parse(document.getElementById('otherId').value)
+    peer.signal(otherId)
+  })
+
+  document.getElementById('send').addEventListener('click', function () {
+    var msgbox = document.getElementById('yourMessage')
+    var yourMessage = msgbox.value
+    peer.send(yourMessage)
+    msgbox.value = ''
+  })
+
+  peer.on('data', function (data) {
+    document.getElementById('messages').textContent += data + '\n'
+  })
+
+  peer.on('stream', function (stream) {
+    const mediaStream = new MediaStream(stream);
+    const video = document.getElementById('video-player');
+    video.srcObject = mediaStream;
+    video.play()
+  })
+})
